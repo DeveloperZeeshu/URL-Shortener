0cbcd265aa0ecd2e1c463154c84c54d2c5e41444
DeveloperZeeshu <itsabbaszeeshaan0604@gmail.com>
2025-06-29 19:06:27 +0530

    Shifted to Express for handling server.

--- server.js
+++ server.js
@@ -1,104 +1,97 @@
-import { readFile, writeFile } from 'fs/promises';
-import http from 'http';
-import crypto from 'crypto';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from 'express'
+import { readFile, writeFile } from 'fs/promises'
+import path from 'path'
+import crypto from 'crypto'
 
-const filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(filename);
-const DATA_FILE = path.join(_dirname, 'data', 'links.json');
+const app = express()
 
-const serveFile = async (res, filepath, contentType) => {
-    try {
-        const data = await readFile(filepath);
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(data);
-    }
-    catch (err) {
-        res.writeHead(404, { 'Content-Type': contentType });
-        res.end(err);
-    }
-}
+const staticPath = path.join(import.meta.dirname)
+const DATA_FILE = path.join(staticPath, 'data', 'links.json')
+app.use(express.static(path.join(staticPath, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
 const loadLinks = async () => {
     try {
-        const data = await readFile(DATA_FILE, 'utf-8');
-        return JSON.parse(data);
+        const data = await readFile(DATA_FILE, 'utf-8')
+        return JSON.parse(data)
     } catch (err) {
         if (err.code === 'ENOENT') {
-            await writeFile(DATA_FILE, JSON.stringify({}));
-            return {};
+            await writeFile(DATA_FILE, JSON.stringify({}))
+            return {}
         }
-        throw err;
+        throw err
     }
 }
 
 const saveLinks = async (links) => {
     try {
-        await writeFile(DATA_FILE, JSON.stringify(links));
+        await writeFile(DATA_FILE, JSON.stringify(links))
     } catch (err) {
-        console.log(err);
+        console.log(err)
     }
 }
 
-const server = http.createServer(async (req, res) => {
-    if (req.method === 'GET') {
-        if (req.url === '/') {
-            return serveFile(res, path.join(_dirname, 'index.html'), 'text/html')
-        }
-        else if (req.url === '/style.css') {
-            return serveFile(res, path.join(_dirname, 'style.css'), 'text/css');
-        }
-        else if (req.url === '/app.js') {
-            return serveFile(res, path.join(_dirname, 'app.js'), 'application/javascript');
-        }
-        else if (req.url === '/links') {
-            const links = await loadLinks();
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            return res.end(JSON.stringify(links));
-        }
-        else {
-            const links = await loadLinks();
-            const shortCode = req.url.slice(1);
-            if (links[shortCode]) {
-                res.writeHead(302, { location: links[shortCode] });
-                return res.end();
-            }
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            return res.end('Page not found');
-        }
+app.get('/', async (req, res) => {
+    try {
+        const filepath = path.join(staticPath, 'views', 'index.html')
+        const file = await readFile(filepath)
+        const links = await loadLinks()
+
+        const content = file.toString().replaceAll(
+            '{{shortened_urls}}',
+            Object.entries(links).map(
+                ([shortCode, url]) =>
+                    `<li><a style='color: #168fbeff; font-weight: 500' href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> -> ${url}</li>`
+            ).join('')
+        )
+
+        return res.send(content)
     }
-
-    if (req.method === 'POST' && req.url === '/shorten') {
-        const links = await loadLinks();
-
-        let body = '';
-        req.on('data', (chunk) => (body += chunk))
-        req.on('end', async () => {
-            console.log(body);
-            const { url, shortCode } = JSON.parse(body);
-            if (!url) {
-                res.writeHead(400, { 'Content-Type': 'text/plain' });
-                return res.end('URL is required');
-            }
-
-            const finalShortCode = shortCode || crypto.randomBytes(4).toString('hex');
-
-            if (links[finalShortCode]) {
-                res.writeHead(409, { 'Content-Type': 'text/plain' });
-                return res.end('Short code already exists, Please choose another.');
-            }
-
-            links[finalShortCode] = url;
-            await saveLinks(links);
-
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ success: true, shortCode: finalShortCode }));
-        })
+    catch (err) {
+        console.log('Error loading page!')
+        return res.status(500).send('Internal server error!')
     }
 })
 
-const PORT = 3002;
-server.listen(PORT, () => console.log(`Server running on ${PORT} port...`));
+app.post('/', async (req, res) => {
+    try {
+        const { url, shortCode } = req.body
+        const links = await loadLinks()
+        const finalShortCode = shortCode || crypto.randomBytes(4).toString('hex')
 
+        if (links[finalShortCode]) {
+            return res.status(409).send('ShortCode already exists. Please choose another.')
+        }
+
+        links[finalShortCode] = url
+        await saveLinks(links)
+
+        return res.redirect('/')
+
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send('Internal server error!')
+    }
+})
+
+app.get('/:shortCode', async (req, res) => {
+    try {
+        const { shortCode } = req.params
+        const links = await loadLinks()
+
+        if (!links[shortCode]) return res.status(404).sendFile(path.join(staticPath, 'views', '404.html'))
+
+        return res.redirect(links[shortCode])
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send('Internal server error!')
+    }
+})
+
+app.use((req, res) => {
+    return res.status(404).sendFile(path.join(staticPath, 'views', '404.html'));
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running at ${PORT} port...`))
 
